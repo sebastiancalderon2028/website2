c00c7683c841d62645ad53ddcd6943c9452e57ac
sebastiancalderon2028 <138073235+sebastiancalderon2028@users.noreply.github.com>
2023-06-30 10:52:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var firstname = "sebastian"
+var lastname = "calderon"
+var number0fsiblings = 2;
+var haspets = true;
+console.log(firstname)
+console.log(lastname)
+console.log(number0fsiblings)
+console.log(haspets)
+
+var number = 0;
+while(number < 15){
+console.log("123456789101112131415");
+number++;
+
+}
+
+
+var button = document.getElementById("hi")
+
+button.addEventListener("click", function(){
+console.log("click");
+
+
+
+});
